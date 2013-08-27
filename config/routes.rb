42f2d7db23a52b0a28e06eaba1b4b1a0c5ac1034
Christian Nelson <winjacan@gmail.com>
2013-08-27 13:58:00 -0700

    redirected root to the proper page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HelloWorld::Application.routes.draw do
+  root :to => redirect('/static_pages/index')
   get "static_pages/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
